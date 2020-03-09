8de9cdd9f51d966bc6ffac09f9de516e5ae1da88
Chaudhary-Hassnain <tajammalhusnain1996@gmail.com>
2020-03-10 00:28:15 +0500

    Task 1: Which of all Star Wars movies has the longest opening crawl (counted by number of characters)?

--- Controllers/StarsController.cs
+++ Controllers/StarsController.cs
@@ -1,10 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using ReactStartApi.DataAccess;
-using ReactStartApi.DataAccess.Models;
 using ReactStartApi.DTOs;
 
 namespace ReactStartApi.Controllers
@@ -15,6 +12,39 @@ namespace ReactStartApi.Controllers
     {
         AppDataContext _context = new AppDataContext();
 
-     
+        /// <summary>
+        /// Gets a film of with max character length
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<QuestionAndAnswerDto> GetQuestionAndAnswers()
+        {
+
+
+
+            List<QuestionAndAnswerDto> listQuestionAndAnswer = new List<QuestionAndAnswerDto>();
+
+            #region 1st question
+            var fileTitleLength = _context.Films
+                .Select(i => new FileTitleLengthDto
+                {
+                    id = i.id,
+                    title = i.title,
+                    opening_crawl = i.opening_crawl,
+                    opening_crawl_length = i.opening_crawl.Length
+                }).OrderByDescending(x => x.opening_crawl_length).Take(1)
+                .AsQueryable();
+            var fileTitleLengthResult = fileTitleLength.First(m => m.opening_crawl_length == (fileTitleLength.Max(e => e.opening_crawl_length)));
+            string filmTitle = fileTitleLengthResult.title;
+
+            QuestionAndAnswerDto objFirstQuestion = new QuestionAndAnswerDto();
+            objFirstQuestion.QuestionTitle = "Which of all Star Wars movies has the longest opening crawl (counted by number  of characters)?";
+            objFirstQuestion.Answer = filmTitle;
+
+            listQuestionAndAnswer.Add(objFirstQuestion);
+            #endregion
+
+                   return listQuestionAndAnswer;
+        }
     }
 }
